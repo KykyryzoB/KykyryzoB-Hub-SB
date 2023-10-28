c880578aadbe96c318d4bca17b17c7e90ecd3db0
KykyryzoB <148477723+KykyryzoB@users.noreply.github.com>
2023-10-28 17:15:25 +0500

    Update KykryzoB.lua

--- KykryzoB.lua
+++ KykryzoB.lua
@@ -805,31 +805,6 @@ Section:NewSlider("Player Gravity", "SliderInfo", 500, 0, function(s) -- 500 (М
     workspace.Gravity = s
 end)
 
-local Section = Tab:NewSection("Infinite Jump")
-
-Section:NewButton("Inf Jump", "ButtonInfo", function()
-    local player = game.Players.LocalPlayer
-local humanoid = player.Character.Humanoid
-
--- Переменная для отслеживания состояния прыжка
-local isJumping = false
-
--- Функция для выполнения прыжка и отталкивания
-local function jumpAndBounce()
-    if not isJumping then
-        isJumping = true
-        humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-    else
-        humanoid.Jump = true
-        humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-    end
-end
-
--- Обработка нажатия клавиши прыжка
-game:GetService("UserInputService").JumpRequest:Connect(jumpAndBounce)
-end
-end)
-
 local Tab = Window:NewTab("Others Hub")
 
 local Section = Tab:NewSection("Guy that exsits hub")
