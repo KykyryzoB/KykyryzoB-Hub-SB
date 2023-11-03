3ade33d9671d1dfcc552f1c51c1c3f33f429e4d9
KykyryzoB <148477723+KykyryzoB@users.noreply.github.com>
2023-11-03 23:45:06 +0500

    Update KykryzoB.lua

--- KykryzoB.lua
+++ KykryzoB.lua
@@ -1,5 +1,19 @@
 local bypass;
     bypass = hookmetamethod(game, "__namecall", function(method, ...) 
+        if getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.Events.Ban then
+            return
+        elseif getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.Events.AdminGUI then
+            return
+        elseif getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.Events.WS then
+            return
+        elseif getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.Events.WS2 then
+            return
+        end
+        return bypass(method, ...)
+    end)
+----------------------------------------------------------------------
+local bypass;
+    bypass = hookmetamethod(game, "__namecall", function(method, ...) 
         if getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.Ban then
             return
         elseif getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.AdminGUI then
